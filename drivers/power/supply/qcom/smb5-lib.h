ca6bc9b1abfc77b409a85262f177b66bd0693bf2
Ashay Jaiswal <ashayj@codeaurora.org>
2018-10-03 11:58:15 -0700

    power: smb5: Disable charger-hw ADC channels before reading USBIN_V
    There could be a possible hardware coupling between USBIN_V channel and
    other voltage based ADC channel which could lead to false USBIN_OV
    interrupts. Fix this by disabling voltage based charger-hw ADC channels
    before reading USBIN_V.
    
    Mot-CRs-fixed: (CR)
    
    Change-Id: I230698c0eeb3a8c646c843a4533f38671484925e
    Signed-off-by: Ashay Jaiswal <ashayj@codeaurora.org>
    Signed-off-by: Guru Das Srinagesh <gurus@codeaurora.org>
    Signed-off-by: Harry Yang <harryy@codeaurora.org>
    Reviewed-on: https://gerrit.mot.com/1392652
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Haijian Ma <mahj8@motorola.com>
    Reviewed-by: Jianqi Yang <yangj@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/1415063
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>

--- drivers/power/supply/qcom/smb5-lib.h
+++ drivers/power/supply/qcom/smb5-lib.h
@@ -149,6 +149,7 @@ enum {
 	WEAK_ADAPTER_WA			= BIT(2),
 	USBIN_OV_WA			= BIT(3),
 	CHG_TERMINATION_WA		= BIT(4),
+	USBIN_ADC_WA			= BIT(5),
 };
 
 enum jeita_cfg_stat {
@@ -530,6 +531,7 @@ struct smb_charger {
 	struct mutex		smb_lock;
 	struct mutex		ps_change_lock;
 	struct mutex		dr_lock;
+	struct mutex            adc_lock;
 	struct mutex		irq_status_lock;
 	spinlock_t		typec_pr_lock;
 	struct mutex		dcin_aicl_lock;
